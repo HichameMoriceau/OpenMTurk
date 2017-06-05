40ce3b22a00a4af75fd54e996d659e6c47c9ace1
HichameMoriceau <moriceau.hichame@gmail.com>
2017-06-05 23:18:43 +0100

    Add basic UI for user to specify orientation of document

--- project/static/scripts/main_template.js
+++ project/static/scripts/main_template.js
@@ -34,10 +34,17 @@ $(document).ready(function(){
 
 	var images = {{ images }};
 	var counter = 0;
+	var orientation = -1;
 
     $(document).keydown(function(e) {
 	console.log('keydown event!!');
+
     switch(e.which) {
+
+		//
+		// NAVIGATION between images
+		//
+
         case 37: // left
 	        if (counter != 0){
 				counter--;
@@ -52,12 +59,49 @@ $(document).ready(function(){
 			console.log('right');
         break;
 
+        // 
+        // ORIENTATION: using [0, 3] digits
+        //
+		case 49: 
+	        orientation = 0;
+			console.log('orientation: ', orientation);
+			$("li:eq(0)").css("color", "red")
+			$("li:eq(1)").css("color", "black")
+			$("li:eq(2)").css("color", "black")
+			$("li:eq(3)").css("color", "black")
+        break;
+
+		case 50: 
+	        orientation = 1;
+			console.log('orientation: ', orientation);
+			$("li:eq(0)").css("color", "black")
+			$("li:eq(1)").css("color", "red")
+			$("li:eq(2)").css("color", "black")
+			$("li:eq(3)").css("color", "black")
+        break;
+
+		case 51: 
+	        orientation = 2;
+			console.log('orientation: ', orientation);
+			$("li:eq(0)").css("color", "black")
+			$("li:eq(1)").css("color", "black")
+			$("li:eq(2)").css("color", "red")
+			$("li:eq(3)").css("color", "black")
+        break;
+
+		case 52: 
+	        orientation = 3;
+			console.log('orientation: ', orientation);
+			$("li:eq(0)").css("color", "black")
+			$("li:eq(1)").css("color", "black")
+			$("li:eq(2)").css("color", "black")
+			$("li:eq(3)").css("color", "red")
+        break;
+
+
         default: return; // exit this handler for other keys
     }
 
-    console.log(counter);
-    console.log(images[counter])
-    console.log(images.length)
     document.getElementById('ex_img').src=images[counter];
 });
 });
