d59e1b2caa1c67c4c568d93c521f720219a922b7
HichameMoriceau <moriceau.hichame@gmail.com>
2017-06-07 23:39:34 +0100

    Fix incorrect strings in labels.

--- project/static/scripts/main_template.js
+++ project/static/scripts/main_template.js
@@ -28,24 +28,25 @@ function select_document_type(doc_types, dt){
 $(document).ready(function(){
 
 	var images = {{ images }};
-	var counter = 0;
-	var selected_bb = -1;
-	var bounding_boxes = [];
-	var document_type = -1;
+	var image_idx = 0;
 	
-	// default orientation: up
+	var bounding_boxes = [];
+	var document_type = 0;
 	var orientation = 0;
+	var selected_bb = 0;
 
 	var canvas = document.getElementById('ex_canvas');
     var ctx = canvas.getContext("2d");
 
+	var doc_types = ['Notebook', 'Form', 'Receipt', 'Letter'];
+	var orientations = ['up', 'right', 'down', 'left'];
 	var bbs_names = ['text_region', 'drawing', 'formula'];
+	
 	var bbs_colors = ['red', 'green', 'yellow'];
 	var bbs_legend = document.getElementById('bounding_box_colors');
-    ctx.strokeStyle=bbs_colors[0];
-
 
 	$.each(bbs_names, function(i){
+
 		var li = $('<li/>')
 			.text(bbs_names[i])
 			.css("background-color", bbs_colors[i])
@@ -55,41 +56,47 @@ $(document).ready(function(){
 
 		li.mousedown(function (e) {
 			console.log('click on ' + bbs_names[i])
-			selected_bb = bbs_names[i];
+			selected_bb = i;
 		});
 
 	})
 
-	var doc_types = ['Notebook', 'Form', 'Receipt', 'Letter'];
-	var doc_type_legend = document.getElementById('document_type');
-
 	$.each(doc_types, function(i){
+
+		var doc_type_legend = document.getElementById('document_type');
 		var key_value = i+1;
+		
 		var li = $('<li/>')
 			.text(doc_types[i] + ' (key: ' + key_value + ')')
 			.appendTo(doc_type_legend);
 	})
-	select_document_type(doc_types, doc_types[0]);
 
-	var orientations = ['up', 'right', 'down', 'left'];
-	var orientation_legend = document.getElementById('orientation');
 
 	$.each(orientations, function(i){
+		
+		var orientation_legend = document.getElementById('orientation');
 		var key_value = doc_types.length+i+1;	
+		
 		var li = $('<li/>')
 			.text(orientations[i] + ' (key: ' + key_value + ')')
 			.css('margin-bottom', '1%')
 			.appendTo(orientation_legend);
 	})
+
+	select_document_type(doc_types, doc_types[0]);
 	select_orientation(0);
 
 	var img = new Image(); 
 	img.onload = function () {
+
+		// resize image but maintain original ratio
 	 	var img_ratio = img.width / img.height;
 	 	var new_width = 500;
 	 	var new_height = new_width / img_ratio;
+		
 		canvas.width = new_width;
 	    canvas.height = new_height;
+	    
 	    ctx.drawImage(img,0,0,img.width,img.height,0,0,new_width,new_height);
 		ctx.strokeStyle=bbs_colors[0];
 		ctx.lineWidth=6;
@@ -97,7 +104,6 @@ $(document).ready(function(){
 
 	img.src = '../static/notes_photos/IMG_20170604_100551.jpg';
 	
-
 	var isDrawing=false;
 	var startX;
 	var startY;
@@ -107,21 +113,17 @@ $(document).ready(function(){
 	var offsetY = canvasOffset.top;
 
 	function handleMouseDown(e) {
-		console.log('offset:')
-		console.log(offsetX, offsetY)
-		console.log('e.client:')
-		console.log(e.clientX, e.clientY)
+
 	    mouseX = parseInt(e.clientX - offsetX);
 	    mouseY = parseInt(e.clientY - offsetY); 
+	    
 	    $("#downlog").html("Down: " + mouseX + " / " + mouseY);
 
 	    // Put your mousedown stuff here
 	    if (isDrawing) {
-	    	console.log('x1, y1')
-	    	console.log(mouseX - startX, mouseY - startY)
 
 	    	bb = {
-	    		"label": selected_bb,
+	    		"label": bbs_names[selected_bb],
 	    		"point_0": [startX, startY],
 	    		"point_1": [mouseX - startX, mouseY - startY],
 	    	}
@@ -132,9 +134,8 @@ $(document).ready(function(){
 	        ctx.strokeRect(startX, startY, mouseX - startX, mouseY - startY);
 	        // ctx.fill();
 	        canvas.style.cursor = "default";
+	    
 	    } else {
-	    	console.log('x0, y0')
-	    	console.log(mouseX, mouseY)
 	        isDrawing = true;
 	        startX = mouseX;
 	        startY = mouseY;
@@ -152,15 +153,12 @@ $(document).ready(function(){
 		console.log('test');
 		$("#bounding_box_colors li").eq(i).mousedown(function (e) {
 
-			console.log('new color!');
-			ctx.strokeStyle=bbs_colors[i];
-
+			ctx.strokeStyle = bbs_colors[i];
 		});
 	})
 
 
     $(document).keydown(function(e) {
-	console.log('keydown event!!');
 
     switch(e.which) {
 
@@ -169,18 +167,18 @@ $(document).ready(function(){
 		//
 
         case 37: // left
-	        if (counter != 0){
-				counter--;
+	        if (image_idx != 0){
+				image_idx--;
 			}
-			img.src = images[counter];
+			img.src = images[image_idx];
 			console.log('left');
         break;
 
         case 39: // right
-	        if (counter < images.length){
-				counter++;
+	        if (image_idx < images.length){
+				image_idx++;
 			}
-			img.src = images[counter];
+			img.src = images[image_idx];
 			console.log('right');
         break;
 
@@ -246,9 +244,13 @@ $(document).ready(function(){
         //
 
         case 13: // enter
+
+        	console.log(orientation);
+        	console.log(orientations);
         	var json_obj = {
-        		"img_path": images[counter],
-        		"orientation": orientation,
+        		"img_path": images[image_idx],
+        		"document_type": doc_types[document_type],
+        		"orientation": orientations[orientation],
         		"bounding_boxes": bounding_boxes
         	}
 
@@ -261,10 +263,10 @@ $(document).ready(function(){
 			        console.log(result);
 			    }
 			});
-			if (counter < images.length){
-				counter++;
+			if (image_idx < images.length){
+				image_idx++;
 			}
-			img.src = images[counter];
+			img.src = images[image_idx];
         break;
 
         default: return; // exit this handler for other keys
