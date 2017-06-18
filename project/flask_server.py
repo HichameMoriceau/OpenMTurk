68aa25383356ab3001490511ee418c3421096fc8
HichameMoriceau <moriceau.hichame@gmail.com>
2017-06-18 13:40:20 +0100

    Add basic authentication method.

--- project/flask_server.py
+++ project/flask_server.py
@@ -18,7 +18,8 @@
 	#
 """
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, Response
+from functools import wraps
 from pymongo import MongoClient
 import glob
 import os
@@ -30,6 +31,31 @@ app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
 client = MongoClient()
 db = client['labels_db'] # use existing database
+
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 
 def get_style_version(dir_path):
 
@@ -165,6 +191,7 @@ def reset():
 		return jsonify(result=300)
 
 @app.route('/')
+@requires_auth
 def index():
 	style_version = get_style_version('static/scripts/js/*')
 	
